d8cc2045eff601157f4cbf5d36e4f905661f62d2
Greyson Mainor <gbmainor98@outlook.com>
2021-01-22 14:30:55 -0500

    got the color change event to work.  Changed my time blocks to military time in order to process it more easily.  Consolidated my addclass and removeclass attributes to one line each to clean up the code a lil bit.

--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,21 @@ $(document).ready(function () {
     //setting my current date and time to the top of my page in the currentDay div.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    //setting variables to call upon later in my functions.
-    var currentTime = moment().format("h a");
-    var timeRow = parseInt($(this).attr("id"));
-    console.log(currentTime)
-    $(".time-block").each(function () {
-        if (timeRow < currentTime) {
-            $(this).removeClass("future", "present");
-            $(this).addClass("past");
-        } else if (timeRow === currentTime) {
-            $(this).removeClass("past", "future");
-            $(this).addClass("present");
-        } else {
-            $(this).removeClass("present", "past");
-            $(this).addClass("future");
-        }
-    });
+    //setting up my color changer based on the time of day.
+    function colorChanger() {
+        var currentTime = moment().hour();
+        console.log(currentTime)
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future", "present").addClass("past");
+            }else if (timeBlock === currentTime) {
+                $(this).removeClass("past", "future").addClass("present");
+            }else {
+                $(this).removeClass("present", "past").addClass("future");
+            }
+        })
+    }
 
     //setting up a save button that actually saves.
     // var timeBlockText = $(this).siblings(".description").val("");
@@ -34,6 +33,15 @@ $(document).ready(function () {
         });
 
      
-    JSON.parse(localStorage.getItem("#hour8"))
+    function load() {
+        var timeBlockText = $(this).siblings(".description").val();
+        var blockId = $(this).parent().attr("id");
+        localStorage.getItem("timeBlockText", JSON.stringify(timeBlockText))
+        localStorage.getItem("blockId", JSON.stringify(blockId))
+    
+    }
+
+    colorChanger();
 
 });
+
