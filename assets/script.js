5354375f17ba3dc8e39b40b9c92efcdd2e0a3f0e
Greyson Mainor <gbmainor98@outlook.com>
2021-01-22 00:59:13 -0500

    added variables to use in my functions for getting the current time as well as isolating the time blocks by their id.

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,13 @@ $(document).ready(function () {
  //setting my current date and time to the top of my page in the currentDay div.
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-    //setting colors for my page based on time of day.
+    //setting variables to call upon later in my functions.
+    var currentTime = moment().format("h a")
+    var hourBlock = parseInt($(this).attr("id").split("hour")[1])
     
+    console.log(currentTime)
+   //setting colors for my page based on time of day.
+$(".time-block").each
 
+    
 });
