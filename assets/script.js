d11fc73401b765c285ecf134dc7c430294b5b4ff
Greyson Mainor <gbmainor98@outlook.com>
2021-01-21 21:58:06 -0500

    added my moment.js in order to reference current time,  added my current time to my webpage, also moved my jquery script to the top of my html page.

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+ //setting my current date and time to the top of my page in the currentDay div.
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    //setting colors for my page based on time of day.
+    
+
+});
