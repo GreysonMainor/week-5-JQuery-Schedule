5bfc6a445629ab7083845a369120b09d77aa38c8
Greyson Mainor <gbmainor98@outlook.com>
2021-01-23 10:01:08 -0500

    re did my color change function so that it only highlights text area.  also added my get local storage at the top.  Changed my index.html to have the time data attribute to help with my color change.  changed my css to keep the text visible in my past time blocks.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
 
+    //Grabbing information from local storage if there is any.
+     $("#hour8 .description").val(localStorage.getItem("hour8"));
+     $("#hour9 .description").val(localStorage.getItem("hour9"));
+     $("#hour10 .description").val(localStorage.getItem("hour10"));
+     $("#hour11 .description").val(localStorage.getItem("hour11"));
+     $("#hour12 .description").val(localStorage.getItem("hour12"));
+     $("#hour13 .description").val(localStorage.getItem("hour13"));
+     $("#hour14 .description").val(localStorage.getItem("hour14"));
+     $("#hour15 .description").val(localStorage.getItem("hour15"));
+     $("#hour16 .description").val(localStorage.getItem("hour16"));
+     $("#hour17 .description").val(localStorage.getItem("hour17"));
+
     //setting my current date and time to the top of my page in the currentDay div.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
@@ -7,36 +19,26 @@ $(document).ready(function () {
     function colorChanger() {
         var currentTime = moment().hour();
         console.log(currentTime)
-        $(".time-block").each(function () {
-            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-            if (timeBlock < currentTime) {
-                $(this).removeClass("future", "present").addClass("past");
-            }else if (timeBlock === currentTime) {
-                $(this).removeClass("past", "future").addClass("present");
-            }else {
-                $(this).removeClass("present", "past").addClass("future");
-            }
+        $(".description").each(function () {
+             var timeBlock = parseInt($(this).attr("time"));
+             console.log(timeBlock)
+             console.log(this)
+              if (timeBlock < currentTime) {
+                  $(this).removeClass("future", "present").addClass("past");
+              }else if (timeBlock === currentTime) {
+                  $(this).removeClass("past", "future").addClass("present");
+              }else {
+                  $(this).removeClass("present", "past").addClass("future");
+              }
         })
     }
-
     //setting up a save button that actually saves.
-        $(".saveBtn").on("click", function () {
-            var timeBlockText = $(this).siblings(".description").val();
-            var blockId = $(this).parent().attr("id");
-
-            localStorage.setItem("timeBlockText", JSON.stringify(timeBlockText))
-            localStorage.setItem("blockId", JSON.stringify(blockId))
-            console.log()
-
-        });
-
-     
-    $(".saveBtn").on("click", function(){
-        var timeBlockText = $(this).siblings(".description").val();
-        var blockId = $(this).parent().attr("id");
+     $(".saveBtn").on("click", function(){
+         var timeBlockText = $(this).parent().attr("id");
+         var blockId = $(this).siblings(".description").val();
         localStorage.setItem(timeBlockText, blockId)
 })
 
     colorChanger();
-
 });
+
